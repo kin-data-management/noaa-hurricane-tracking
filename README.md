47b94f7ffd489800e4a192b63f2eeb723cfcd9ed
benjmainmillunchick <ben.millunchick@kin.com>
2023-07-12 10:23:43 -0400

    newest data script and readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # noaa-hurricane-tracking
+
+This repository contains the latest data from NOAA. The data is replicated from S3 to GitHub on a daily basis.
+
+## Contents
+The repository contains the following files:
+- replicate_s3_folder.yaml: A Git action that replicates the S3 folder which belongs to Kin Insurance to this GitHub repo (STORMNAME/geojson/).
+- newest_data_geojson.py: A Python script which is triggered by that finds the latest data for each storm and copies it to a folder called newest_data for each storm (STORMNAME/newest_data/).
+
+## Usage
+
+This repo is intended to be ingested by Looker. The newest_data folders will have consistand names, but updated data. This is to simplify the connection looker needs to make. 
+
---
+++ newest_data_geojson.py
@@ -0,0 +1,41 @@
+import os
+import re
+import shutil
+
+def get_storm_paths():
+    paths = []
+    cwd = os.getcwd()
+    for path, subdirs, files in os.walk(cwd):
+        if re.match(r"^.*/geojson", path):
+            paths.append(path.replace(cwd + "/", ""))
+    return paths
+
+def get_files_in_folder(folder_path):
+  files = []
+  for file in os.listdir(folder_path):
+    if os.path.isfile(os.path.join(folder_path, file)):
+      files.append(file)
+  return files
+
+for storm in get_storm_paths():
+    file_names = get_files_in_folder(storm)
+
+    newest_data_wsp34knt120hr_5km = sorted([file_name for file_name in file_names if "wsp34knt120hr_5km" in file_name], reverse=True)[0]
+    newest_data_wsp34knt120hr_halfDeg = sorted([file_name for file_name in file_names if "wsp34knt120hr_halfDeg" in file_name], reverse=True)[0]
+    newest_data_wsp50knt120hr_5km = sorted([file_name for file_name in file_names if "wsp50knt120hr_5km" in file_name], reverse=True)[0]
+    newest_data_wsp50knt120hr_halfDeg = sorted([file_name for file_name in file_names if "wsp50knt120hr_halfDeg" in file_name], reverse=True)[0]
+    newest_data_wsp64knt120hr_5km = sorted([file_name for file_name in file_names if "wsp64knt120hr_5km" in file_name], reverse=True)[0]
+    newest_data_wsp64knt120hr_halfDeg = sorted([file_name for file_name in file_names if "wsp64knt120hr_halfDeg" in file_name], reverse=True)[0]
+    newest_data_lin_5day = sorted([file_name for file_name in file_names if "5day_lin" in file_name], reverse=True)[0]
+    newest_data_pgn_5day = sorted([file_name for file_name in file_names if "5day_pgn" in file_name], reverse=True)[0]
+    newest_data_pts_5day = sorted([file_name for file_name in file_names if "5day_pts" in file_name], reverse=True)[0]
+    newest_data_lin = sorted([file_name for file_name in file_names if ("lin" in file_name and "5day" not in file_name)], reverse=True)[0]
+    newest_data_pts = sorted([file_name for file_name in file_names if ("pts" in file_name and "5day" not in file_name)], reverse=True)[0]
+    newest_data_radii = sorted([file_name for file_name in file_names if ("radii" in file_name and "5day" not in file_name)], reverse=True)[0]
+
+    storm_name = storm.split('/')[0]
+    for file in [newest_data_wsp34knt120hr_5km, newest_data_wsp34knt120hr_halfDeg, newest_data_wsp50knt120hr_5km, newest_data_wsp50knt120hr_halfDeg, newest_data_wsp64knt120hr_5km, newest_data_wsp64knt120hr_halfDeg, newest_data_lin_5day, newest_data_pgn_5day, newest_data_pts_5day, newest_data_lin, newest_data_pts, newest_data_radii]:
+        src = f"{storm}/{file}"
+        dest = f"{storm_name}/newest_data/{file}"
+        os.makedirs(os.path.dirname(dest), exist_ok=True)
+        shutil.copy(src, dest)
